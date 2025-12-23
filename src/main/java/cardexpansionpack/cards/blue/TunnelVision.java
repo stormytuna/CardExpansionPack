24fd952b671a9f44ef4247855d08af4ce352cf89
stormytuna <stormytuna@outlook.com>
2025-12-23 20:32:27 +0000

    add tunnel vision

--- src/main/java/cardexpansionpack/cards/blue/TunnelVision.java
+++ src/main/java/cardexpansionpack/cards/blue/TunnelVision.java
@@ -1,44 +1,39 @@
 package cardexpansionpack.cards.blue;
 
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
-import com.megacrit.cardcrawl.actions.defect.ChannelAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.orbs.Lightning;
 import com.megacrit.cardcrawl.powers.LockOnPower;
 
 import cardexpansionpack.cards.BaseCard;
 import cardexpansionpack.util.CardStats;
 
-public class Polarization extends BaseCard {
-    public static final String ID = makeID("Polarization");
+public class TunnelVision extends BaseCard {
+    public static final String ID = makeID("TunnelVision");
 
     private static final CardStats INFO = new CardStats(
         CardColor.BLUE,
         CardType.SKILL,
-        CardRarity.COMMON,
+        CardRarity.UNCOMMON,
         CardTarget.ENEMY,
         1
     );
 
-    private static final int LOCKON_AMOUNT = 2;
-    private static final int LOCKON_UPGRADE = 1;
-
-    public Polarization() {
+    public TunnelVision() {
         super(ID, INFO);
-        setMagic(LOCKON_AMOUNT, LOCKON_UPGRADE);
+        setCostUpgrade(0);
+        setExhaust(true);
     }
 
     @Override
     public AbstractCard makeCopy() {
-        return new Polarization();
+        return new TunnelVision();
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        addToBot(new ChannelAction(new Lightning()));
-        addToBot(new ApplyPowerAction(m, p, new LockOnPower(m, this.magicNumber)));
+        addToBot(new ApplyPowerAction(m, p, new LockOnPower(m, 99)));
     }
 }
 
