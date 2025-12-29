bced9633bf8b39bba78730939e2f1e4bc9f9db56
stormytuna <stormytuna@outlook.com>
2025-12-29 17:10:21 +0000

    add salvage

---
+++ src/main/java/cardexpansionpack/cards/colorless/Salvage.java
@@ -0,0 +1,39 @@
+package cardexpansionpack.cards.colorless;
+
+import com.megacrit.cardcrawl.actions.common.BetterDiscardPileToHandAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import cardexpansionpack.cards.BaseCard;
+import cardexpansionpack.util.CardStats;
+
+public class Salvage extends BaseCard {
+    public static final String ID = makeID("Salvage");
+
+    private static final CardStats INFO = new CardStats(
+        CardColor.COLORLESS,
+        CardType.SKILL,
+        CardRarity.UNCOMMON,
+        CardTarget.SELF,
+        1
+    );
+
+    private static final int CARDS_TO_RETRIEVE = 2;
+
+    public Salvage() {
+        super(ID, INFO);
+        setMagic(CARDS_TO_RETRIEVE);
+        setCostUpgrade(0);
+    }
+
+    @Override
+    public AbstractCard makeCopy() {
+        return new Salvage();
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new BetterDiscardPileToHandAction(CARDS_TO_RETRIEVE));
+    }
+}
