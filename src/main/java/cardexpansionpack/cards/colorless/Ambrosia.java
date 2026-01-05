8b87b7bc698fecfbe4370b84ee7ab8c9d88d40b5
stormytuna <stormytuna@outlook.com>
2026-01-05 07:25:56 +0000

    change ambrosia effect

--- src/main/java/cardexpansionpack/cards/colorless/Ambrosia.java
+++ src/main/java/cardexpansionpack/cards/colorless/Ambrosia.java
@@ -1,13 +1,16 @@
 package cardexpansionpack.cards.colorless;
 
-import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.evacipated.cardcrawl.modthespire.lib.SpireInsertPatch;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch2;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePostfixPatch;
 import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.actions.common.EmptyDeckShuffleAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
+import basemod.ReflectionHacks;
 import cardexpansionpack.cards.BaseCard;
-import cardexpansionpack.powers.AmbrosiaPower;
 import cardexpansionpack.util.CardStats;
 
 public class Ambrosia extends BaseCard {
@@ -36,9 +39,36 @@ public class Ambrosia extends BaseCard {
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
+        // Doing draw pile shuffling for the DrawCardAction so our postfix further down works properly
+        if (magicNumber > p.drawPile.size()) {
+            addToBot(new EmptyDeckShuffleAction());
+        }
         addToBot(new DrawCardAction(magicNumber));
-        if (!p.hasPower(AmbrosiaPower.ID)) {
-            addToBot(new ApplyPowerAction(p, p, new AmbrosiaPower(p)));
+        UpgradeAmbrosiaDraws.upgrade = true;
+    }
+
+    @SpirePatch2(clz = AbstractPlayer.class, method = "draw", paramtypez = {int.class})
+    public static class UpgradeAmbrosiaDraws {
+        public static boolean upgrade = false;
+
+        @SpireInsertPatch(rloc = 12, localvars = "c")
+        public static void patch(AbstractCard c) {
+            if (upgrade && c.canUpgrade()) {
+                c.upgrade();
+                c.superFlash();
+                c.applyPowers();
+            }
+        }
+    }
+
+    // Setting to false once entire action is finished as we can draw multiple and we want to upgrade all, not just the first
+    @SpirePatch2(clz = DrawCardAction.class, method = "update")
+    public static class ResetAmbrosiaUpgradeBool {
+        @SpirePostfixPatch
+        public static void patch(DrawCardAction __instance) {
+            if (__instance.isDone) {
+                UpgradeAmbrosiaDraws.upgrade = false;
+            }
         }
     }
 }
