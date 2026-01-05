3a7e9ad2b59fc77a985d70732c00493a0edeeba8
stormytuna <stormytuna@outlook.com>
2026-01-05 08:30:39 +0000

    fix blindside adding itself on end of turn and not adding itself when some discard cards are played

--- src/main/java/cardexpansionpack/cards/green/Blindside.java
+++ src/main/java/cardexpansionpack/cards/green/Blindside.java
@@ -5,6 +5,7 @@ import com.evacipated.cardcrawl.modthespire.lib.SpirePostfixPatch;
 import com.megacrit.cardcrawl.actions.AbstractGameAction.AttackEffect;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.actions.common.DiscardAction;
+import com.megacrit.cardcrawl.actions.common.DiscardSpecificCardAction;
 import com.megacrit.cardcrawl.actions.utility.DiscardToHandAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
@@ -45,16 +46,30 @@ public class Blindside extends BaseCard {
         addToBot(new DamageAction(m, info, AttackEffect.SLASH_VERTICAL));
     }
 
+    private static void fetchBlindsides() {
+        for (AbstractCard c : AbstractDungeon.player.discardPile.group) {
+            if (c instanceof Blindside) {
+                AbstractDungeon.actionManager.addToBottom(new DiscardToHandAction(c));
+            }
+        }
+    }
+
     @SpirePatch2(clz = DiscardAction.class, method = "update")
     public static class OnDiscardCard {
         @SpirePostfixPatch
-        public static void patch(DiscardAction __instance) {
+        public static void patch(DiscardAction __instance, boolean ___endTurn) {
+            if (__instance.isDone && !___endTurn) {
+                fetchBlindsides();
+            }
+        }
+    }
+
+    @SpirePatch2(clz = DiscardSpecificCardAction.class, method = "update")
+    public static class OnDiscardCard2 {
+        @SpirePostfixPatch
+        public static void patch(DiscardSpecificCardAction __instance) {
             if (__instance.isDone) {
-                for (AbstractCard c : AbstractDungeon.player.discardPile.group) {
-                    if (c instanceof Blindside) {
-                        AbstractDungeon.actionManager.addToTop(new DiscardToHandAction(c));
-                    }
-                }
+                fetchBlindsides();
             }
         }
     }
