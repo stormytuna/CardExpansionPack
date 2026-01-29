2989df0bc928aff7e4643e7073d7cca56c03d2e7
stormytuna <stormytuna@outlook.com>
2026-01-29 16:42:56 +0000

    change workshop image, fix underhanded tactics proccing for mp clients with together in spire

--- src/main/java/cardexpansionpack/powers/UnderhandedTacticsPower.java
+++ src/main/java/cardexpansionpack/powers/UnderhandedTacticsPower.java
@@ -1,7 +1,9 @@
 package cardexpansionpack.powers;
 
+import com.evacipated.cardcrawl.mod.stslib.patches.SoulboundPatch.AbstractPlayer_isCursed;
 import com.megacrit.cardcrawl.actions.common.DrawCardAction;
 import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.powers.AbstractPower;
 import com.megacrit.cardcrawl.powers.ArtifactPower;
 
@@ -34,7 +36,7 @@ public class UnderhandedTacticsPower extends BasePower {
 
     @Override
     public void onApplyPower(AbstractPower power, AbstractCreature target, AbstractCreature source) {
-        if (numDebuffsAppliedThisTurn < amount && power.type == PowerType.DEBUFF && source == owner && target != owner && !target.hasPower(ArtifactPower.POWER_ID)) {
+        if (numDebuffsAppliedThisTurn < amount && power.type == PowerType.DEBUFF && source == AbstractDungeon.player && target != owner && !target.hasPower(ArtifactPower.POWER_ID)) {
             numDebuffsAppliedThisTurn++;
             flash();
             addToBot(new DrawCardAction(1));
